2862849033ae7f3f3494bb40711318916cb0753e
Geraldine <hello@geraldiner.com>
2021-11-21 12:50:06 -0500

    initial commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function updateAllRepos() {
 			sort: "updated",
 			per_page: "100",
 		});
-		const repos = res.data.filter(r => r.name !== "geraldiner" && r.name !== "projects-readme");
+		const repos = res.data.filter(r => r.name !== "geraldiner" && r.name !== "projects-readme" && r.name !== "projects-readme-tutorial");
 		for (let i = 0; i < repos.length; i++) {
 			const { name } = repos[i];
 			updateReadMe(name);
